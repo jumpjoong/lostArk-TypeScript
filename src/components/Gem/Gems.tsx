bad36bf8d7dcf521f30e3bb61cf296117129f0a4
kimhyunjoong <100519998+khj0196@users.noreply.github.com>
2023-07-21 21:34:08 +0900

    no gems fix

--- src/components/Gem/Gems.tsx
+++ src/components/Gem/Gems.tsx
@@ -21,12 +21,13 @@ function Gems({idx, obj}: GemsProps) {
     setTip(!tip);
   }
   //gems에 있는 Tooltip deep clone
-  const json = JSON.parse(JSON.stringify(obj.Tooltip))
-  //변수명 json을 parse **안하면 객체가 아닌 스트링임(이벤트 보석인 경우 길이가 8, 일반 보석일 경우 7)
+  const json = JSON.parse(JSON.stringify(obj.Tooltip));
+  //변수명 json을 parse **안하면 객체가 아닌 스트링임
   //아...jsonParse 일반 전설 = 6, 일반 영웅 = 7, 이벤트 영웅 = 8 의 길이를 가지고 있음
-  const jsonParse = JSON.parse(json)
-  //객체로 만든걸 깊게 들어가기 (이벤트 캐릭일 경우 Element_005로 접근 해야함);
+  const jsonParse = JSON.parse(json);
+  //객체로 만든걸 깊게 들어가기 (이벤트 캐릭일 경우 Element_005로 접근 해야함)
   let gemsDetails = null
+  //보석이 없는 경우 예외 처리 했음
   if (
     (jsonParse && Object.keys(jsonParse).length === 6 && jsonParse.Element_004.value.Element_001) ||
     (jsonParse && Object.keys(jsonParse).length === 7 && jsonParse.Element_004.value.Element_001) ||
@@ -36,13 +37,14 @@ function Gems({idx, obj}: GemsProps) {
       (jsonParse && Object.keys(jsonParse).length === 6 && jsonParse.Element_004.value.Element_001) ||
       (jsonParse && Object.keys(jsonParse).length === 7 && jsonParse.Element_004.value.Element_001) ||
       (jsonParse && Object.keys(jsonParse).length === 8 && jsonParse.Element_005.value.Element_001);
+  } else {
+    return null;
   }
-  console.log( Object.keys(jsonParse))
   //스킬 이름 자르기
   const skillName = gemsDetails !== null && gemsDetails.slice(gemsDetails.indexOf('>')+1, gemsDetails.lastIndexOf(('<')));
   //스킬 설명 자르기
   const skillDescription = gemsDetails !== null && gemsDetails.slice(gemsDetails.indexOf('</FONT>')+8);
-  //이벤트 보석을 가진 캐릭터 구분 해야함
+  //반환
     if(obj.Grade === "전설") {
       return <li data-idx={idx} style={legend} onMouseEnter={mouse} onMouseLeave={mouseOut} >
       <img src={`${obj.Icon}`} alt="보석" />
